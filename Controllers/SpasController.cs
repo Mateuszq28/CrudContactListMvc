6dc505be28ce7b0493a424ad95833dbe9a1ca51c
mateusz_pikus <mateuszq28@gmail.com>
2024-08-16 18:53:06 +0200

    context in cotroller

--- Controllers/SpasController.cs
+++ Controllers/SpasController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CrudContactListMvc.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CrudContactListMvc.Controllers
 {
     public class SpasController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public SpasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
