b2831cdc190086c7370f5564ee97bed47258f86d
mateusz_pikus <mateuszq28@gmail.com>
2024-08-16 17:44:41 +0200

    add context to spa controller

--- Controllers/SpaController.cs
+++ Controllers/SpaController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CrudContactListMvc.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CrudContactListMvc.Controllers
 {
     public class SpaController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public SpaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+
     }
 }
