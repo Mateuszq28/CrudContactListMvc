3a6ab086cca4bb6f96da532a337d37746a379de1
mateusz_pikus <mateuszq28@gmail.com>
2024-08-17 17:33:09 +0200

    default index view simps

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace CrudContactListMvc.Data
         public DbSet<CrudContactListMvc.Models.Subcategory> Subcategory { get; set; } = default!;
         public DbSet<CrudContactListMvc.Models.Contact> Contact { get; set; } = default!;
         public DbSet<CrudContactListMvc.Models.Spa> Spa { get; set; } = default!;
+        public DbSet<CrudContactListMvc.Models.Simp> Simp { get; set; } = default!;
     }
 }
