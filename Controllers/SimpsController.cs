33bdbb2902ff1fecd5404d00969893717d21eea7
mateusz_pikus <mateuszq28@gmail.com>
2024-08-17 16:11:05 +0200

    adding SPIMP CONTROLLER

---
+++ Controllers/SimpsController.cs
@@ -0,0 +1,294 @@
+﻿using CrudContactListMvc.Data;
+using CrudContactListMvc.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudContactListMvc.Controllers
+{
+    public class SimpsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SimpsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MAIN PAGE
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // =====================================
+        // BLOCK FOR INTERACTIONS WITH CONTACT
+        // =====================================
+
+        // GET: Contacts
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Contact.Include(c => c.Category).Include(c => c.Subcategory);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Contacts/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+
+        // PoST: Contacts/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        {
+            return View("Index", await _context.Contact.Where(j => (j.Name.Contains(SearchPhrase) ||
+                                                                     j.Surname.Contains(SearchPhrase) ||
+                                                                     j.Email.Contains(SearchPhrase) ||
+                                                                     //j.BirthDate.Date.Equals(DateTime.Parse(SearchPhrase).Date) ||
+                                                                     j.Phone.Contains(SearchPhrase))).ToListAsync());
+        }
+
+        // GET: Contacts/Details/5
+        [Authorize]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contact
+                .Include(c => c.Category)
+                .Include(c => c.Subcategory)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        // GET: Contacts/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            // string for condition to show drop-down subcategory menu
+            Category expectedCategory = _context.Category.Where(m => m.Id == 1).First();
+            ViewBag.DropDownCategory = expectedCategory.Name;
+            //ViewBag.DropDownCategory = "Business";
+
+
+            // other category - it is used to show text field instead of drop-down
+            Category otherCategoryName = _context.Category.Where(m => m.Id == 3).First();
+            ViewBag.otherCategoryName = otherCategoryName.Name;
+            //ViewBag.otherCategoryName = "Other";
+
+
+            // Send emails to View. This are emails from the Database
+            // Reason: They must be UNIQUE
+            List<string> emails = _context.Contact.Select(m => m.Email).ToList();
+            ViewBag.emails = emails;
+
+
+
+
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name");
+
+
+
+            // Working, but pointless - it should be done in Views JavaScript
+
+            /*
+            var subcategories = _context.Category.ToList();
+            // Create a list of SelectListItem
+            var subcategoryList = subcategories.Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.Name
+            }).ToList();
+            // Add a new option
+            subcategoryList.Insert(0, new SelectListItem { Value = "", Text = "" });
+            // Pass the list to the SelectList constructor
+            ViewData["SubcategoryId"] = new SelectList(subcategoryList, "Value", "Text");
+            */
+
+
+
+
+            // Doesn't working
+
+            //SelectList slSubcategory = new SelectList(_context.Subcategory, "Id", "Name");
+            //slSubcategory.Insert(0, new SelectListItem { Value = "", Text = "" });
+            //ViewData["SubcategoryId"] = slSubcategory;
+
+
+
+            // NULL values in Selection List should be implemented in Views JavaScript
+            ViewData["SubcategoryId"] = new SelectList(_context.Subcategory.Where(m => m.CategoryId == 1), "Id", "Name");
+
+
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Create(IFormCollection form)
+        {
+            string newSubcatName = form["NewCategory"];
+
+            Contact contact = new Contact
+            {
+                //Id = int.Parse(form["Id"]),
+                Email = form["Email"],
+                Name = form["Name"],
+                Surname = form["Surname"],
+                Password = form["Password"],
+                Phone = form["Phone"],
+                BirthDate = DateTime.Parse(form["BirthDate"]),
+                CategoryId = int.Parse(form["CategoryId"])
+            };
+
+            // SubcategoryId can be in at least 3 different states
+            if (contact.CategoryId == 3)
+            {
+                int catId = 3;
+                Subcategory newSub = new Subcategory
+                {
+                    Name = newSubcatName,
+                    CategoryId = catId
+                };
+                _context.Add(newSub);
+                await _context.SaveChangesAsync();
+
+                contact.SubcategoryId = newSub.Id;
+                //contact.SubcategoryId = _context.Subcategory.Where(m => m.Name == newSubcatName).Last().Id;
+            }
+            else if (contact.CategoryId == 2)
+            {
+                contact.SubcategoryId = null;
+            }
+            else
+            {
+                contact.SubcategoryId = int.Parse(form["SubcategoryId"]);
+            }
+
+
+
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(contact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name", contact.CategoryId);
+            ViewData["SubcategoryId"] = new SelectList(_context.Subcategory, "Id", "Name", contact.SubcategoryId);
+            return View(contact);
+        }
+
+        // GET: Contacts/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contact.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Id", contact.CategoryId);
+            ViewData["SubcategoryId"] = new SelectList(_context.Subcategory, "Id", "Id", contact.SubcategoryId);
+            return View(contact);
+        }
+
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Email,Name,Surname,Password,Phone,BirthDate,CategoryId,SubcategoryId")] Contact contact)
+        {
+            if (id != contact.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contact);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactExists(contact.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Id", contact.CategoryId);
+            ViewData["SubcategoryId"] = new SelectList(_context.Subcategory, "Id", "Id", contact.SubcategoryId);
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contact
+                .Include(c => c.Category)
+                .Include(c => c.Subcategory)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contact = await _context.Contact.FindAsync(id);
+            if (contact != null)
+            {
+                _context.Contact.Remove(contact);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContactExists(int id)
+        {
+            return _context.Contact.Any(e => e.Id == id);
+        }
+    }
+}
